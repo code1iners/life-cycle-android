d40bd00c9554428869796aa5ddcdc7b5c1105d93
Ethan <48429547+wy0353@users.noreply.github.com>
2021-02-13 03:28:05 +0900

    Added Timber library and init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning Life Cycle by Build test app.
---
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Timber.i("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+}
